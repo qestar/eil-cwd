c81cad2571b35c36b1b18fc592487fbc4a40ebf8
chengjiyuan@dnect.cn <chengjiyuan@dnect.cn>
2023-08-31 22:43:44 +0800

    testCWD2

--- trainer/zeroth_phase.py
+++ trainer/zeroth_phase.py
@@ -63,6 +63,7 @@ def incremental_train_and_eval_zeroth_phase(the_args, epochs, b1_model, ref_mode
             tg_optimizer.zero_grad()
             # Forward the samples in the deep networks
             outputs = b1_model(inputs)
+
             # Loss CWD
             aux_loss = DecorrelateLossClass(reject_threshold=1, ddp=False)
             features = F.normalize(outputs, p=2, dim=-1)
